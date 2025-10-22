1a3b9274bde7f56703c829b697688bd317010c06
lkj <leee1846@gmail.com>
2025-10-22 16:39:34 +0900

    feat: reset css 적용

--- packages/ui/src/styles/reset.ts
+++ packages/ui/src/styles/reset.ts
@@ -1,84 +1,131 @@
 import { css } from '@emotion/react';
 
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
 export const resetStyles = css`
-  /* Modern CSS Reset */
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-
-  html {
-    -webkit-text-size-adjust: 100%;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  body {
-    min-height: 100vh;
-    line-height: 1.5;
-    text-rendering: optimizeSpeed;
-  }
-
-  /* Remove default list styles */
-  ul,
-  ol {
-    list-style: none;
-  }
-
-  /* Make images easier to work with */
-  img,
-  picture,
-  video,
-  canvas,
-  svg {
-    display: block;
-    max-width: 100%;
-  }
-
-  /* Inherit fonts for inputs and buttons */
-  input,
-  button,
-  textarea,
-  select {
-    font: inherit;
-  }
-
-  /* Remove button styles */
-  button {
-    background: none;
-    border: none;
-    cursor: pointer;
-  }
-
-  /* Remove link styles */
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-
-  /* Improve text wrapping */
-  p,
+  html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
   h1,
   h2,
   h3,
   h4,
   h5,
-  h6 {
-    overflow-wrap: break-word;
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
   }
-
-  /* Remove default form element styles */
-  input,
-  textarea,
-  select {
-    border: none;
-    outline: none;
+  /* HTML5 display-role reset for older browsers */
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
   }
-
-  /* Improve table styles */
+  body {
+    line-height: 1;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
   table {
     border-collapse: collapse;
     border-spacing: 0;
