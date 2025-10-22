d4898b508a4a8e407eaca9c7a8b1efef491a8f14
lkj <leee1846@gmail.com>
2025-10-22 11:36:13 +0900

    core: 페이지 컴포넌트 코드 스플리팅 예제 적용

--- apps/example/src/router.tsx
+++ apps/example/src/router.tsx
@@ -1,26 +1,55 @@
+import { Suspense, lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
-import { HomePage } from '@/pages/HomePage';
-import { AboutPage } from '@/pages/AboutPage';
-import { UserProfilePage } from '@/pages/UserProfilePage';
 import { ROUTES } from '@/constants/routes';
+const HomePage = lazy(() =>
+  import('@/pages/HomePage').then((module) => ({ default: module.HomePage }))
+);
+const AboutPage = lazy(() =>
+  import('@/pages/AboutPage').then((module) => ({ default: module.AboutPage }))
+);
+const UserProfilePage = lazy(() =>
+  import('@/pages/UserProfilePage').then((module) => ({
+    default: module.UserProfilePage,
+  }))
+);
+
+const SuspenseFallback = () => {
+  return <div>Loading...</div>;
+};
 
 export const router = createBrowserRouter([
   {
     path: ROUTES.HOME.path,
-    element: <App />,
+    element: (
+      <Suspense fallback={<SuspenseFallback />}>
+        <App />
+      </Suspense>
+    ),
     children: [
       {
         index: true,
-        element: <HomePage />,
+        element: (
+          <Suspense fallback={<SuspenseFallback />}>
+            <HomePage />
+          </Suspense>
+        ),
       },
       {
         path: ROUTES.ABOUT.path,
-        element: <AboutPage />,
+        element: (
+          <Suspense fallback={<SuspenseFallback />}>
+            <AboutPage />
+          </Suspense>
+        ),
       },
       {
         path: ROUTES.USER_PROFILE.path,
-        element: <UserProfilePage />,
+        element: (
+          <Suspense fallback={<SuspenseFallback />}>
+            <UserProfilePage />
+          </Suspense>
+        ),
       },
     ],
   },
