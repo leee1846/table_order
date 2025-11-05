6d7d46ef0cfd81bce4024dcb9a1b12f42ac3d259
leemirae <mirae@handorder.co.kr>
2025-11-05 18:47:02 +0900

    core : icon 추가

--- apps/example/src/pages/HomePage/Home/index.tsx
+++ apps/example/src/pages/HomePage/Home/index.tsx
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import { ButtonExample, MainTitle } from '@repo/ui';
+import { ButtonExample, MainTitle } from '@repo/ui/components';
 
 import { ROUTES } from '@/constants/routes';
 import { UserList } from '@/pages/HomePage/UserList';
 import { TYPOGRAPHY } from '@repo/ui';
+import { SearchIcon } from '@repo/ui/icons';
 
 export const Home = () => {
   const navigate = useNavigate();
@@ -11,8 +12,9 @@ export const Home = () => {
   return (
     <div>
       <h2>Welcome to Home Page</h2>
-      <p css={TYPOGRAPHY.MT_1}>Main Title 1</p>
+      <p style={TYPOGRAPHY.MT_4}>Main Title 1</p>
       <MainTitle />
+      <SearchIcon color="red" width={50} height={70} />
       <div style={{ marginTop: '20px' }}>
         <ButtonExample onClick={() => navigate(ROUTES.ABOUT.path)}>
           About 페이지로 이동 →
