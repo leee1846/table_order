849b83944ae0bbc5b46997a68346d81d8e51db28
leemirae <mirae@handorder.co.kr>
2025-11-07 11:03:46 +0900

    core : 버튼 컴포넌트 추가

--- packages/ui/src/components/BasicButton/index.tsx
+++ packages/ui/src/components/BasicButton/index.tsx
@@ -2,37 +2,11 @@
 
 import type { ReactNode } from 'react';
 import * as S from './basicButton.styles';
-
-type TVariant =
-  | 'Solid_Navy_S'
-  | 'Solid_Navy_M'
-  | 'Solid_Navy_L'
-  | 'Solid_Navy_XL'
-  | 'Solid_Navy_2XL'
-  | 'Outline_Navy_S'
-  | 'Outline_Navy_M'
-  | 'Outline_Navy_L'
-  | 'Outline_Navy_XL'
-  | 'Outline_Navy_2XL'
-  | 'Solid_Sky_Blue_S'
-  | 'Solid_Sky_Blue_M'
-  | 'Solid_Sky_Blue_L'
-  | 'Solid_Sky_Blue_XL'
-  | 'Solid_Sky_Blue_2XL'
-  | 'Outline_Grey_S'
-  | 'Outline_Grey_M'
-  | 'Outline_Grey_L'
-  | 'Outline_Grey_XL'
-  | 'Outline_Grey_2XL'
-  | 'Solid_Grey_S'
-  | 'Solid_Grey_M'
-  | 'Solid_Grey_L'
-  | 'Solid_Grey_XL'
-  | 'Solid_Grey_2XL';
+import type { VariantKey } from './basicButton.styles';
 
 interface Props {
   children: ReactNode;
-  variant: TVariant;
+  variant?: VariantKey;
   disabled?: boolean;
   onClick?: () => void;
 }
@@ -43,12 +17,18 @@ export const BasicButton = ({
   disabled = false,
   onClick,
 }: Props) => {
+  const handleClick = (_e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!disabled && onClick) {
+      onClick();
+    }
+  };
+
   return (
     <S.ButtonStyle
       type="button"
       variant={variant}
       disabled={disabled}
-      onClick={onClick}
+      onClick={handleClick}
     >
       {children}
     </S.ButtonStyle>
