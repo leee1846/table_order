bfe24ab5b350ea2cd0dc112526ad87873c63cee7
lkj <leee1846@gmail.com>
2025-11-10 11:48:06 +0900

    feat: 카테고리 목록, 메뉴 목록 라우터 세팅

--- apps/admin/src/router.tsx
+++ apps/admin/src/router.tsx
@@ -1,8 +1,22 @@
 import { Suspense, lazy } from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate, redirect } from 'react-router-dom';
 import { ROUTES } from '@/constants/routes';
 
-const LoginPage = lazy(() => import('@/pages/LoginPage'));
+const SettingSidebar = lazy(() =>
+  import('@/components/settings/SidebarLayout').then((module) => ({
+    default: module.SidebarLayout,
+  }))
+);
+const CategoriesPage = lazy(() =>
+  import('@/pages/settings/CategoriesPage').then((module) => ({
+    default: module.CategoriesPage,
+  }))
+);
+const CategoryMenusPage = lazy(() =>
+  import('@/pages/settings/CategoryMenusPage').then((module) => ({
+    default: module.CategoryMenusPage,
+  }))
+);
 
 const SuspenseFallback = () => {
   return <div>Loading...</div>;
@@ -10,11 +24,40 @@ const SuspenseFallback = () => {
 
 export const router = createBrowserRouter([
   {
-    path: ROUTES.LOGIN.path,
+    path: ROUTES.SETTINGS.path,
     element: (
       <Suspense fallback={<SuspenseFallback />}>
-        <LoginPage />
+        <SettingSidebar />
       </Suspense>
     ),
+    children: [
+      {
+        // /settings → /settings/categories
+        index: true,
+        element: <Navigate to={ROUTES.SETTINGS.CATEGORIES.path} replace />,
+      },
+      {
+        path: ROUTES.SETTINGS.CATEGORIES.path,
+        element: (
+          <Suspense fallback={<SuspenseFallback />}>
+            <CategoriesPage />
+          </Suspense>
+        ),
+      },
+      {
+        // /settings/categories/:id → 바로 menus로 리디렉트
+        path: `${ROUTES.SETTINGS.CATEGORIES.path}/:id`,
+        loader: ({ params }) =>
+          redirect(ROUTES.SETTINGS.CATEGORY_MENUS.generate(params.id!)),
+      },
+      {
+        path: ROUTES.SETTINGS.CATEGORY_MENUS.path,
+        element: (
+          <Suspense fallback={<SuspenseFallback />}>
+            <CategoryMenusPage />
+          </Suspense>
+        ),
+      },
+    ],
   },
 ]);
