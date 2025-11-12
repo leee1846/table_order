be14aa5507c460f5f35bdbe9405de8480fcc6201
lkj <leee1846@gmail.com>
2025-11-12 11:15:14 +0900

    feat: 메뉴 추가 > header 스타일 적용

--- apps/admin/src/pages/settings/CategoryMenusPage/MenuManageModal/index.tsx
+++ apps/admin/src/pages/settings/CategoryMenusPage/MenuManageModal/index.tsx
@@ -1,5 +1,5 @@
 import * as S from '@/pages/settings/CategoryMenusPage/MenuManageModal/menuManageModal.style';
-import { ChevronForwardIcon, CloseIcon } from '@repo/ui/icons';
+import { ChevronForwardIcon, CloseIcon, EditIcon } from '@repo/ui/icons';
 import { theme } from '@repo/ui';
 import { BasicSetting } from '@/pages/settings/CategoryMenusPage/MenuManageModal/BasicSetting';
 import { OptionSetting } from '@/pages/settings/CategoryMenusPage/MenuManageModal/OptionSetting';
@@ -10,21 +10,25 @@ export const MenuManageModal = () => {
 
   return (
     <S.Container>
-      <div>
-        <p>
-          메뉴 관리 <div /> <span>{MENU_NAME}</span>
-          <ChevronForwardIcon
-            color={theme.colors.grey[600]}
-            width={24}
-            height={24}
-          />
-          <span>메뉴 추가</span>
-        </p>
+      <S.Header>
+        <S.Titles>
+          <p>메뉴 관리</p>
+          <span />
+          <div>
+            <p>{MENU_NAME}</p>
+            <ChevronForwardIcon
+              color={theme.colors.grey[600]}
+              width={24}
+              height={24}
+            />
+            <p>메뉴 추가</p>
+          </div>
+        </S.Titles>
 
         <button type="button">
           <CloseIcon width={42} height={42} color={theme.colors.grey[500]} />
         </button>
-      </div>
+      </S.Header>
 
       <BasicSetting />
 
--- apps/admin/src/pages/settings/CategoryMenusPage/MenuManageModal/menuManageModal.style.ts
+++ apps/admin/src/pages/settings/CategoryMenusPage/MenuManageModal/menuManageModal.style.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { TYPOGRAPHY } from '@repo/ui';
 
 export const Container = styled.div`
   position: absolute;
@@ -6,6 +7,53 @@ export const Container = styled.div`
   left: 13.125rem;
   right: 0;
   height: 100%;
+  padding: 40px 24px 40px 30px;
   overflow-y: auto;
-  background-color: red;
+  background-color: ${({ theme }) => theme.colors.grey[50]};
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & > div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const Titles = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > p {
+    color: ${({ theme }) => theme.colors.grey[800]};
+    ${TYPOGRAPHY.MT_1}
+  }
+
+  & > span {
+    width: 0.125rem;
+    height: 1.25rem;
+    margin: 0 11px;
+    background-color: ${({ theme }) => theme.colors.semantic[800]};
+  }
+
+  & > div {
+    display: flex;
+    align-items: center;
+
+    & > svg {
+      margin-top: 4px;
+    }
+
+    & > p {
+      color: ${({ theme }) => theme.colors.grey[600]};
+      ${TYPOGRAPHY.ST_1}
+    }
+
+    & > p:last-child {
+      color: ${({ theme }) => theme.colors.primary[500]};
+      ${TYPOGRAPHY.ST_1}
+    }
+  }
 `;
