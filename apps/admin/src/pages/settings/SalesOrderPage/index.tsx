8e2f7bd5288c8a8b5cf5d15c3d2eb802c1c158d3
lkj <leee1846@gmail.com>
2025-11-14 16:51:40 +0900

    feat: 매출관리 > 주문내역 header 항목 구성

--- apps/admin/src/pages/settings/SalesOrderPage/index.tsx
+++ apps/admin/src/pages/settings/SalesOrderPage/index.tsx
@@ -1,10 +1,20 @@
-import { Calender, Pagination } from '@repo/ui/components';
+import { Calender, Dropdown, Pagination } from '@repo/ui/components';
+import { CalendarMonthIcon } from '@repo/ui/icons';
+import { theme } from '@repo/ui';
 import { useState } from 'react';
 
 export const SalesOrderPage = () => {
   const [showCalender, setShowCalender] = useState<boolean>(false);
   const [startDate, setStartDate] = useState<string>('');
   const [endDate, setEndDate] = useState<string>('');
+
+  const orderStatusOptions = [
+    { value: 'all', label: '오늘' },
+    { value: 'yesterday', label: '어제' },
+    { value: 'thisWeek', label: '이번주' },
+    { value: 'thisMonth', label: '이번달' },
+    { value: '3Months', label: '3개월' },
+  ];
 
   const onSelectDate = (startDate: string, endDate: string) => {
     setStartDate(startDate);
@@ -13,7 +23,29 @@ export const SalesOrderPage = () => {
 
   return (
     <div>
-      <div onClick={() => setShowCalender(true)}>클릭하세요</div>
+      <p>
+        매출 관리
+        <span />
+        <span>주문내역</span>
+      </p>
+
+      <div>
+        <button type="button" onClick={() => setShowCalender(true)}>
+          <CalendarMonthIcon
+            width={32}
+            height={32}
+            color={theme.colors.grey[700]}
+          />
+          <div>
+            {startDate} ~ {endDate}
+          </div>
+        </button>
+        <Dropdown
+          options={orderStatusOptions}
+          value={null}
+          onChange={() => {}}
+        />
+      </div>
       <Pagination totalPages={10} currentPage={1} onPageChange={() => {}} />
       {showCalender && (
         <Calender
