eacd8773f3ff0d98c495f17876c08eed7efd653e
DESKTOP-0PMF6MO\user <mirae@archseoul.co.kr>
2025-12-30 16:12:32 +0900

    feat : SSE 주문 변경

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -4,8 +4,14 @@ import { disconnectSse, initializeSseConnection } from '@/utils/sseConnection';
 import { useSSE } from '@repo/feature/hooks';
 import type { ISseMessage } from '@repo/api/types';
 import { SSE_KEYS } from '@/constants/keys';
+import { useQueryClient } from '@repo/api/tanstack-query';
+import { queryKeys } from '@repo/api/queries';
+import { useAuth } from '@/hooks/useAuth';
 
 const App = () => {
+  const queryClient = useQueryClient();
+  const { shopCode } = useAuth();
+
   useEffect(() => {
     initializeSseConnection();
 
@@ -15,9 +21,17 @@ const App = () => {
   }, []);
 
   const { data } = useSSE.useSSEData<ISseMessage>(SSE_KEYS.MAIN_CONNECTION);
+
   useEffect(() => {
-    console.log('??', data);
-  }, [data]);
+    console.log('SSE', data);
+
+    // ORDER 타입 SSE를 받으면 매장 전체 주문 정보 재조회
+    if (data?.type === 'ORDER' && shopCode) {
+      queryClient.invalidateQueries({
+        queryKey: queryKeys.orders.currentTableList(shopCode),
+      });
+    }
+  }, [data, shopCode, queryClient]);
 
   return (
     <div>
