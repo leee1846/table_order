a16c7cdc190c7c1aaa84dd2131c77e7d292b2724
lkj <leee1846@gmail.com>
2026-02-20 18:34:51 +0900

    feat: Q/A 버전 업데이트

--- apps/admin/src/pages/settings/MiscellaneousPage/Network/index.tsx
+++ apps/admin/src/pages/settings/MiscellaneousPage/Network/index.tsx
@@ -174,7 +174,7 @@ export const Network = ({ shopNetwork, onChange }: NetworkProps) => {
           {/* TODO: 삭제 예정 */}
           <p>
             Q/A 버전
-            <span>1.0.5</span>
+            <span>1.0.6</span>
           </p>
           <p>
             {t('최신 버전')}
