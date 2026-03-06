d2e2a30567b50317ce5cd89c5dc99881bd209a37
leemirae <mirae@archseoul.co.kr>
2026-03-06 11:23:06 +0900

    fix : 모든 다이아로그 안 닫힘. 코드 원복

--- packages/ui/src/components/ModalBackground/index.tsx
+++ packages/ui/src/components/ModalBackground/index.tsx
@@ -8,9 +8,17 @@ interface Props {
   children: React.ReactNode;
 }
 
-export const ModalBackground = ({ position = 'center', children }: Props) => {
+export const ModalBackground = ({ position = 'center', children, onClick }: Props) => {
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    if (onClick) {
+      onClick();
+    }
+  };
+
+
   return (
-    <S.Container position={position}>
+    <S.Container position={position} onClick={handleClick}>
       <div onClick={(e) => e.stopPropagation()}>{children}</div>
     </S.Container>
   );
