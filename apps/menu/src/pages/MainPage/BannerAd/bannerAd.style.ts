8d9796d059fd4c754200e59b672387170a11dbe9
lkj <leee1846@gmail.com>
2026-04-06 18:13:29 +0900

    feat: 배너 광고 퍼블리싱 적용

---
+++ apps/menu/src/pages/MainPage/BannerAd/bannerAd.style.ts
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  padding-bottom: 30px;
+`;
+
+export const ImgContainer = styled.div`
+  width: 46.875rem;
+  height: 8.4375rem;
+  flex-shrink: 0;
+  aspect-ratio: 50/9;
+  border-radius: 0.5rem;
+  overflow: hidden;
+
+  & .banner-ad-swiper {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const SlideImage = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 0.5rem;
+`;
